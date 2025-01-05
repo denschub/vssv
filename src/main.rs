f6e4056183ddfb3537a17459023a7b6bf0e38089
Dennis Schubert <mail@dennis-schubert.de>
2025-01-05 09:10:26 +0100

    Allow manually specifying the number of threads

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use sqlx::{
 use tokio::{net::TcpListener, signal};
 
 use tracing::info;
-use vssv::{routes::build_router, Cli, ServerState};
+use vssv::{routes::build_router, settings::Settings, ServerState};
 
 /// Listens to SIGINT (aka ctrl-c) and SIGTERM and completes whenever one of
 /// those signals happen.
@@ -34,30 +34,43 @@ async fn shutdown_signal() {
 }
 
 /// Creates a [PgPool] if possible. The pool has its max_connections value set
-/// to the number of CPUs available.
+/// to mirror the tokio worker thread count.
 pub async fn get_db_pool(connect_options: PgConnectOptions) -> Result<PgPool, sqlx::Error> {
     PgPoolOptions::new()
         .max_connections(
-            num_cpus::get()
+            tokio::runtime::Handle::current()
+                .metrics()
+                .num_workers()
                 .try_into()
-                .expect("number of CPU cores should fit into an u32"),
+                .expect("num_workers to be less than 2^32"),
         )
         .connect_with(connect_options)
         .await
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
+fn main() -> anyhow::Result<()> {
+    let settings = Settings::parse();
+
+    let mut rt = tokio::runtime::Builder::new_multi_thread();
+    if let Some(threads) = settings.threads {
+        rt.worker_threads(threads);
+    }
+
+    rt.enable_all()
+        .build()?
+        .block_on(async { run(settings).await })
+}
+
+async fn run(settings: Settings) -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let db_pool = get_db_pool(cli.database_url).await?;
+    let db_pool = get_db_pool(settings.database_url).await?;
     sqlx::migrate!().run(&db_pool).await?;
 
     let router = build_router(ServerState { database: db_pool });
-    let listener = TcpListener::bind(cli.listen_addr).await?;
+    let listener = TcpListener::bind(settings.listen_addr).await?;
 
-    info!("Will start to listen on `{}`...", cli.listen_addr);
+    info!("Will start to listen on `{}`...", settings.listen_addr);
     axum::serve(
         listener,
         router.into_make_service_with_connect_info::<SocketAddr>(),
---
+++ src/settings.rs
@@ -0,0 +1,20 @@
+use std::net::SocketAddr;
+
+use sqlx::postgres::PgConnectOptions;
+
+#[derive(Debug, clap::Parser)]
+#[clap(about, version, propagate_version = true)]
+pub struct Settings {
+    /// The database URL to connect to. Needs to be a valid PostgreSQL
+    /// connection URL, like `postgres://postgres@127.0.0.1/vssv`
+    #[clap(long, short, env = "DATABASE_URL")]
+    pub database_url: PgConnectOptions,
+
+    /// The Socket Address the server should listen on
+    #[clap(long, short, env = "LISTEN_ADDR", default_value = "[::1]:3000")]
+    pub listen_addr: SocketAddr,
+
+    /// Limits the number of threads used - defaults to the number of CPU cores
+    #[clap(long, env = "THREADS")]
+    pub threads: Option<usize>,
+}
